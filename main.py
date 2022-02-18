98f9261e75a28369cc5d31d340b50e2381c8df43
RoscaLeRat <noamane.fr11ans@gmail.com>
2022-02-18 21:58:35 +0100

    OMG SA MARCHE

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import string
-import sys
+from sys import * 
+import os
 from time import sleep
 from pathlib import *
-print("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦\n♦ Bienvenue dans le pack script de ©ROSCA. \n•[0] Comment setup les scripts dans un projet py :).\n •[1] setup script par défaut. \n •[2] setup script tkinder \n •[9] Quitter :'( \n♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦")
+print("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦\n♦ Bienvenue dans le pack script de ©ROSCA. \n•[0] Comment setup les scripts dans un projet py :).\n •[1] setup script par défaut. \n •[2] setup script tkinder \n •[4] Quitter :'( \n♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦")
 H = int(input("Votre choix est:"))
 print("Vous avez choisi le script:",H)
 print("Merci d'attendre le chargement du script:",H,"...")
@@ -10,15 +11,20 @@ sleep(2)
 
 if H == 0:
     print("♦Comment setup mes scripts ?")
-#faire le truc chiant pour créer un script dans un autre script (file-creator.py)
+
 if H == 1:
     print("♦Bienvenue dans l'installateur du pack N°1:")
-    Path_s = input("Entre le chemin d'acces de votre:")
-    myfile = Path(Path_s)
-    file = open(Path_s,'main.py','w+')
+    Path_s = input("Entrez le chemin d'acces de votre:")
+    
+    file_name = "main.py"
 
-    myfile.touch(exist_ok=True)
-    print(myfile)
+    acces_file = os.path.join(Path_s, file_name)
+
+    file1 = open(acces_file, "w")
+    file1.write("print(\"caca\")")
+    file1.close()
+    
+  
 
 if H == 2:
     print("♦Comment setup mes scripts ?")
@@ -27,4 +33,5 @@ if H == 9:
     sleep(1)
     exit()
 if H > 4:
+    print("♦Aucun pack-script trouver.♦")
     exit()
